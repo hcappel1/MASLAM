22b7a9d92fb67344698b1d8328ed1f87a4586c64
hcappel1 <hencappel1@gmail.com>
2020-10-02 15:06:14 -0500

    included function for computing paths. Still need to test and visualize

--- frontier_pkg/src/choose_frontier.cpp
+++ frontier_pkg/src/choose_frontier.cpp
@@ -21,13 +21,20 @@ class FrontierPt{
 public:
 
 	FrontierPt(){
-
+		SourceInitialization();
 	}
 
 	geometry_msgs::Pose pose;
 	vector< shared_ptr<FrontierPt> > neighbors;
 	bool valid;
 	string key; 
+
+	void SourceInitialization(){
+		pose.position.x = 3.5;
+		pose.position.y = 12.0;
+		valid = false;
+		key = "source";
+	}
 };
 
 class ChooseFrontier{
@@ -35,7 +42,8 @@ class ChooseFrontier{
 private:
 	ros::NodeHandle nh;
 	ros::Publisher frontier_pub;
-	ros::Publisher neighbors_pub;
+	ros::Publisher frontier_pts_pub;
+	ros::Publisher valid_frontier_pts_pub;
 
 	geometry_msgs::PoseArray optimal_frontier_pts;
 	vector< shared_ptr<FrontierPt> > frontier_queue;
@@ -47,10 +55,14 @@ private:
 
 
 public:
+
+
 	ChooseFrontier(){
 		ROS_INFO("choose frontier object created");
 		frontier_pub = nh.advertise<geometry_msgs::PoseStamped>("chosen_frontier_pt", 1000);
-		neighbors_pub = nh.advertise<geometry_msgs::PoseArray>("neighbor_frontier_pts", 1000);
+		frontier_pts_pub = nh.advertise<geometry_msgs::PoseArray>("frontier_queue", 1000);
+		valid_frontier_pts_pub = nh.advertise<geometry_msgs::PoseArray>("valid_frontier_queue", 1000);
+
 	}
 
 	bool ServiceCallback(frontier_pkg::ChoiceMsg::Request &req,
@@ -59,7 +71,8 @@ public:
 		optimal_frontier_pts = req.optimal_frontier_pts;
 		CreateFrontierQueue();
 		GetNeighbors();
-		NeighborsTest();
+		ComputePaths(frontier_queue[0]);
+		cout << "number of paths: " << source_paths.size() << endl;
 
 		res.success = true;
 	}
@@ -70,10 +83,13 @@ public:
 
 			shared_ptr<FrontierPt> new_frontier_pt = shared_ptr<FrontierPt>( new FrontierPt );
 			new_frontier_pt->pose = optimal_frontier_pts.poses[i];
+			new_frontier_pt->pose.orientation.w = 1.0;
 			new_frontier_pt->valid = false;
 			new_frontier_pt->key = (to_string(optimal_frontier_pts.poses[i].position.x) + "-" + to_string(optimal_frontier_pts.poses[i].position.y));
 			frontier_queue.push_back(new_frontier_pt);
 		}
+		shared_ptr<FrontierPt> source = shared_ptr<FrontierPt>( new FrontierPt );
+		frontier_queue.push_back(source);
 	}
 
 	void GetNeighbors(){
@@ -84,8 +100,74 @@ public:
 				if (frontier_queue[i]->key != frontier_queue[j]->key){
 
 					double distance = sqrt(pow(frontier_queue[i]->pose.position.x - frontier_queue[j]->pose.position.x,2) + pow(frontier_queue[i]->pose.position.y - frontier_queue[j]->pose.position.y,2));
-					if (distance < 3.0){
+					if (distance < 2.0){
 						frontier_queue[i]->neighbors.push_back(frontier_queue[j]);
+					}
+				}
+			}
+		}
+	}
+
+	bool FrontierValid( shared_ptr<FrontierPt> frontier_pt){
+
+		for (int i = 0; i < frontier_pt->neighbors.size(); i++){
+
+			if (frontier_pt->neighbors[i]->key == "source"){
+				return true;
+			}
+			else{
+				for (int j = 0; j < frontier_pt->neighbors[i]->neighbors.size(); j++){
+
+					if (frontier_pt->neighbors[i]->neighbors[j]->key == "source"){
+						return true;
+					}
+					else{
+						for (int k = 0; k < frontier_pt->neighbors[i]->neighbors[j]->neighbors.size(); k++){
+							if (frontier_pt->neighbors[i]->neighbors[j]->neighbors[k]->key == "source"){
+								return true;
+							}
+							else{
+								continue;
+							}
+						}
+					}
+				}
+			}
+			
+		}
+		return false;
+	}
+
+	void ComputePaths( shared_ptr<FrontierPt> candidate_pt){
+		vector< shared_ptr<FrontierPt> > source_path;
+		source_path.push_back(candidate_pt);
+
+		for (int i = 0; i < candidate_pt->neighbors.size(); i++){
+
+			vector< shared_ptr<FrontierPt> > source_path_one = source_path;
+			source_path_one.push_back(candidate_pt->neighbors[i]);
+
+			if (candidate_pt->neighbors[i]->key == "source"){
+				source_paths.push_back(source_path_one);
+			}
+			
+			for (int j = 0; j < candidate_pt->neighbors[i]->neighbors.size(); j++){
+
+				vector< shared_ptr<FrontierPt> > source_path_two = source_path_one;
+				source_path_two.push_back(candidate_pt->neighbors[i]->neighbors[j]);
+
+				if (candidate_pt->neighbors[i]->neighbors[j]->key == "source"){
+					source_paths.push_back(source_path_two);
+				}
+
+				for (int k = 0; k < candidate_pt->neighbors[i]->neighbors[j]->neighbors.size(); k++){
+
+					vector< shared_ptr<FrontierPt> > source_path_three = source_path_two;
+					source_path_three.push_back(candidate_pt->neighbors[i]->neighbors[j]->neighbors[k]);
+					
+					if (candidate_pt->neighbors[i]->neighbors[j]->neighbors[k]->key == "source"){
+						source_paths.push_back(source_path_three);
+				
 					}
 				}
 			}
@@ -109,9 +191,41 @@ public:
 
 		while (ros::ok()){
 			frontier_pub.publish(chosen_frontier_pt);
-			neighbors_pub.publish(neighbor_frontier_pts);
+			//neighbors_pub.publish(neighbor_frontier_pts);
 		}
 	}
+
+	void FrontierValidTest(){
+
+		geometry_msgs::PoseArray valid_frontier_queue;
+		valid_frontier_queue.header.stamp = ros::Time::now();
+		valid_frontier_queue.header.frame_id = "map";
+		optimal_frontier_pts.header.stamp = ros::Time::now();
+		optimal_frontier_pts.header.frame_id = "map";
+
+
+
+		for (int i = 0; i < frontier_queue.size(); i++){
+			bool valid = FrontierValid(frontier_queue[i]);
+			if (valid == true){
+				cout << "frontier point valid" << endl;
+				valid_frontier_queue.poses.push_back(frontier_queue[i]->pose);
+			}
+		}
+
+		while (ros::ok()){
+			frontier_pts_pub.publish(optimal_frontier_pts);
+			valid_frontier_pts_pub.publish(valid_frontier_queue);
+		}
+
+
+	}
+
+	void ComputePathTest(){
+		
+	}
+
+
 
 
 };
